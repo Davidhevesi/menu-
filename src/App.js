97621b21a1638f468bc053201b12258b0218f4ce
David Hevesi <davidhevesi2008@gmail.com>
2022-08-31 17:37:13 -0400

    add images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import items from './data'
+import Categories from './Categories'
+import Menu from './Menu'
 
 function App() {
+  const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories] = useState([])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        {items.map((menuItems) => {})}
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React from 'react'
 
-const Menu = () => {
-  return <div>MENU COmponent</div>;
-};
+const Menu = ({ items }) => {
+  return (
+    <div className="section-center">
+      {items.map((menuItem) => {
+        const { id, title, img, desc } = menuItem
+        return (
+          <article key={id} className="menu-item">
+            <img src={img} alt={title} className="photo" />
+          </article>
+        )
+      })}
+    </div>
+  )
+}
 
-export default Menu;
+export default Menu
